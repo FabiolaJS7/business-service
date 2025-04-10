d8d2acfcbf76b044c6cabc5a524314f63522973a
Monica Fabiola Garcia Saravia <mgarcsar@emeal.nttdata.com>
2025-04-09 23:51:49 -0500

    agregando log a customer connector

--- src/main/java/com/bootcamp/business_service/connector/CustomerConnector.java
+++ src/main/java/com/bootcamp/business_service/connector/CustomerConnector.java
@@ -3,9 +3,6 @@ package com.bootcamp.business_service.connector;
 import com.bootcamp.business_service.util.JsonTransferUtil;
 import com.bootcamp.commons.bean.customers.CustomerRequest;
 import com.bootcamp.commons.bean.customers.CustomerResponse;
-import io.swagger.v3.core.util.Json;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -26,22 +23,28 @@ public class CustomerConnector {
 
     // Endpoint de customer API para traer todos los customers
     public Flux<CustomerResponse> getAllCustomers() {
+        log.info("API getAllCustomers RQ.");
         return webClient.get()
                 .uri("/api/customers")
                 .retrieve()
                 .bodyToFlux(CustomerResponse.class)
-                .doOnError(error -> log.error("Error while fetching customers: {}", error.getMessage())); // Log de errores
+                .doOnNext(response -> log.info("API getAllCustomers RS: {}",
+                        JsonTransferUtil.objectToJson(response)))
+                .doOnError(error -> log.error("API error getAllCustomers: {}", error.getMessage()));
 
 
     }
 
     // Endpoint de customer API para traer customer by id
     public Mono<CustomerResponse> getCustomerById(String customerId) {
+        log.info("API getCustomerById RQ: {}", customerId);
         return webClient.get()
                 .uri("/api/customers/" + customerId)
                 .retrieve()
                 .bodyToMono(CustomerResponse.class)
-                .doOnError(error -> log.error("Error while getting customer by id: {}", error.getMessage()));
+                .doOnNext(response -> log.info("API getCustomerById RS: {}",
+                        JsonTransferUtil.objectToJson(response)))
+                .doOnError(error -> log.error("API error getCustomerById: {}", error.getMessage()));
     }
 
     // Endpoint de customer API para crear un customer
@@ -55,7 +58,7 @@ public class CustomerConnector {
                         .bodyToMono(CustomerResponse.class)
                         .doOnNext(response -> log.info("API createCustomer RS: {}",
                                 JsonTransferUtil.objectToJson(response)))
-                        .doOnError(error -> log.error("Error API createCustomer: {}", error.getMessage()))
+                        .doOnError(error -> log.error("API error createCustomer: {}", error.getMessage()))
                 );
     }
 }
